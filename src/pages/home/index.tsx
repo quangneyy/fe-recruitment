dbb4ee12b999b59cff86096e9e10156bfecb5772
LeonRoy06 <114150434+LeonRoy06@users.noreply.github.com>
2023-11-14 17:28:00 +0700

    FeatuaredArticles

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import styles from 'styles/client.module.scss';
 import SearchClient from '@/components/client/search.client';
 import JobCard from '@/components/client/card/job.card';
 import CompanyCard from '@/components/client/card/company.card';
-
+import FeaturedArticles from '@/components/client/card/FeaturedArticles';
 const HomePage = () => {
     return (
         <div className={`${styles["container"]} ${styles["home-section"]}`}>
@@ -15,6 +15,8 @@ const HomePage = () => {
             <div style={{ margin: 50 }}></div>
             <Divider />
             <JobCard />
+            <Divider />
+            <FeaturedArticles />
         </div>
     )
 }
