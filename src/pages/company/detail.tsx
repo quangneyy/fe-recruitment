d54412f6b92af0687db42d24dbb5af5f814c99b5
lechiphong <lechiphongxxx@gmail.com>
2023-12-05 21:50:03 +0700

    newfe

--- src/pages/company/detail.tsx
+++ src/pages/company/detail.tsx
@@ -7,7 +7,11 @@ import parse from 'html-react-parser';
 import { Col, Divider, Row, Skeleton } from "antd";
 import { EnvironmentOutlined } from "@ant-design/icons";
 import { convertSlug } from "@/config/utils";
-
+import { AiFillEnvironment } from "react-icons/ai";
+import { AiFillDollarCircle } from "react-icons/ai";
+import { FaUser } from "react-icons/fa";
+import { FaCodeBranch } from "react-icons/fa";
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const ClientCompanyDetailPage = (props: any) => {
     const [companyDetail, setCompanyDetail] = useState<ICompany | null>(null);
@@ -50,74 +54,81 @@ const ClientCompanyDetailPage = (props: any) => {
     }
     return (
         <div className={`${styles["container"]} ${styles["detail-job-section"]}`}>
-            {isLoading ?
-                <Skeleton />
-                :
-                <Row gutter={[20, 20]}>
-                    {companyDetail && companyDetail._id &&
-                        <>
-                            <Col span={24} md={16}>
-                            <div className={styles["company-info"]}>
-                                <div>
-                                    <img
-                                        alt="example"
-                                        src={`${import.meta.env.VITE_BACKEND_URL}/images/company/${companyDetail?.logo}`}
-                                    />
+          {isLoading ? (
+            <Skeleton />
+          ) : (
+            <Row gutter={[20, 20]}>
+              {companyDetail && companyDetail._id && (
+                <>
+                  <Col span={24} md={16}>
+                    <div className={styles["company-info"]}>
+                      <div>
+                        <img
+                          alt="example"
+                          src={`${import.meta.env.VITE_BACKEND_URL}/images/company/${companyDetail?.logo}`}
+                        />
+                      </div>
+                      <div className={`${styles["header"]} ${styles["custom-font-style"]}`}>
+                        {companyDetail.name}
+                      </div>
+                    </div>
+                    <div className={styles["location"]}>
+                      <EnvironmentOutlined style={{ color: "#58aaab" }} />&nbsp;{companyDetail?.address}
+                    </div>
+                    <Divider />
+                    {parse(companyDetail?.description ?? "")}
+                  </Col>
+                  <Col span={24} md={8}>
+                    <div className={styles["company"]}>
+                      <div className={styles["jobs"]}>
+                        {companyJobs && companyJobs.length > 0 ? (
+                          <>
+                            <h2 className={`${styles["job-list-title"]} `}>
+                              {`${companyJobs.length} Công Việc`}
+                            </h2>
+                            <div className={styles["job-list"]}>
+                              {companyJobs.map((job) => (
+                                <div
+                                  key={job._id}
+                                  className={`${styles["job-container"]} ${styles["red-border"]}`}
+                                  onClick={() => handleViewDetailJob(job)}
+                                >
+                                  <div className={styles["job-header"]}>
+                                    <p>
+                                      <h2 className={`${styles["custom-font-style"]}`}>{job.name}</h2>
+                                    </p>
+                                    <div className={styles["company"]}>
+                                      <div>
+                                        <img
+                                          alt="example"
+                                          src={`${import.meta.env.VITE_BACKEND_URL}/images/company/${companyDetail?.logo}`}
+                                          className={styles["company-logo"]}
+                                        />
+                                      </div>
+                                      <div className={`${styles["custom-font-style"]}`}>{companyDetail?.name}</div>
+                                    </div>
+                                  </div>
+                                  <p className={`${styles["custom-font-style"]}`}><FaCodeBranch /> Kỹ Năng: {job.skills.join(", ")}</p>
+                                  <p className={`${styles["custom-font-style"]}`}><AiFillEnvironment /> Địa Điểm: {job.location}</p>
+                                  <p className={`${styles["custom-font-style"]}`}><AiFillDollarCircle /> Mức Lương:{job.salary} VND</p>
+                                  <p className={`${styles["custom-font-style"]}`}><FaUser /> Số Lượng: {job.quantity}</p>
+                                  <p className={`${styles["custom-font-style"]}`}><FaStarHalfAlt /> Level: {job.level}</p>
                                 </div>
-                                <div className={styles["header"]}>
-                                    {companyDetail.name}
-                                </div>
+                              ))}
                             </div>
-                                <div className={styles["location"]}>
-                                    <EnvironmentOutlined style={{ color: '#58aaab' }} />&nbsp;{(companyDetail?.address)}
-                                </div>
-                                <Divider />
-                                {parse(companyDetail?.description ?? "")}
-                            </Col>
-                            <Col span={24} md={8}>
-                                <div className={styles["company"]}>
-                                    <div className={styles["jobs"]}>
-                                        {companyJobs && companyJobs.length > 0 ? (
-                                            <>
-                                            <h2 className={styles["job-list-title"]}>{`${companyJobs.length} Công Việc`}</h2>
-                                            <div className={styles["job-list"]}>  
-                                                                                       
-                                                {companyJobs.map((job) => (
-                                                <div key={job._id} className={`${styles["job-container"]} ${styles["red-border"]}`}onClick={() => handleViewDetailJob(job)}>
-                                                    <div className={styles["job-header"]}>
-                                                    <h2>{job.name}</h2>
-                                                    <div className={styles["company"]}>
-                                                <div>
-                                                    <img
-                                                    alt="example"
-                                                    src={`${import.meta.env.VITE_BACKEND_URL}/images/company/${companyDetail?.logo}`}
-                                                    className={styles["company-logo"]}
-                                                    />
-                                                </div>
-                                                <div>{companyDetail?.name}</div>
-                                                </div>
-                                                    </div>
-                                                    <p>Kỹ Năng: {job.skills.join(', ')}</p>
-                                                    <p>Địa Điểm: {job.location}</p>
-                                                    <p>Mức Lương: ${job.salary}</p>
-                                                    <p>Số Lượng: {job.quantity}</p>
-                                                    <p>Level: {job.level}</p>
-                                                    
-                                                </div>
-                                                ))}
-                                            </div>
-                                            </>
-                                        ) : (
-                                            <p></p>
-                                        )}
-                                        </div>
-                                    </div>        
-                            </Col>
-                        </>
-                    }
-                </Row>
-            }
+                          </>
+                        ) : (
+                          <p></p>
+                        )}
+                      </div>
+                    </div>
+                  </Col>
+                </>
+              )}
+            </Row>
+          )}
         </div>
-    )
+      );
+      
 }
 export default ClientCompanyDetailPage;
